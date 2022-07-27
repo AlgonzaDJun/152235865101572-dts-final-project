042bd2b8f5507dc89f06aad6df3bdbb8fc2fefac
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2022-07-27 13:46:40 +0700

    setup header replace <a> with NavLink and href with to

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid py-3 ">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="Products">
+                  Product
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="About">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="Contact">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+            <NavLink className="navbar-brand fw-bold" to="/">
+              Apple Mart
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Header
+export default Header;
