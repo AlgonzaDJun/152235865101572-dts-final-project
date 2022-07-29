bdeae9f1242d5ff66417e4d2c21c356431763e48
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2022-07-29 11:20:36 +0700

    delete older version of redux

--- src/components/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import addItem from "./addItem";
-
-const rootReducer = combineReducers({
-    // Add your reducers here
-    addItem
-})
-
-export default rootReducer;
